1ac16a3c7866d207b91aa28c644d315855822fa2
Sumiet Gore <sumeetgore96@gmail.com>
2023-06-19 01:48:58 +0200

    Added vision board to home page

--- app/page.js
+++ app/page.js
@@ -5,6 +5,7 @@ import Link from "next/link"
 import Image from "next/image"
 import Footer from "@/components/shared/footer"
 import Section from "@/components/ui/section"
+import { OpenSource, Leaf, Help, Innovation } from "@/components/icons"
 
 export const metadata = {
   title: "Sumiet Gore",
@@ -20,13 +21,48 @@ export default function Home() {
           <div className="container flex flex-col lg:flex-row gap-8 items-center max-w-5xl">
             <div className="h-52 w-52 rounded-full bg-muted border-border border">
               <div className="relative h-40 w-40 mx-auto my-auto">
-                <Image src="/memoji.png" fill unoptimized />
+                <Image src="/memoji.png" fill unoptimized alt="Sumiet Gore memoji" />
               </div>
             </div>
             <div className="items-center lg:items-start flex flex-col">
               <span className="px-3 py-1 rounded-2xl bg-muted text-muted-foreground text-sm border border-border">Open to Internship Opportunities</span>
-              <h1 className="mt-2 text-6xl font-bold text-center lg:text-left">Hi! I am Sumiet Gore</h1>
-              <p className="mt-6 text-muted-foreground text-center lg:text-left">Embedded Systems Engineering student at University of Turku and Technisxhe Universtat Berlin.</p>
+              <h1 className="mt-2 text-6xl font-bold text-center lg:text-left">Namaste</h1>
+              <p className="mt-6 text-muted-foreground text-center lg:text-left max-w-2xl">I am Sumiet (a.k.a. Sumit) Gore, an Embedded Systems Engineering student at University of Turku and Technische Universtat Berlin.</p>
+            </div>
+          </div>
+        </Section>
+        <Section>
+          <div className="container flex flex-col gap-8">
+            <div><h1 className="text-4xl font-bold">Vision Board</h1></div>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-12">
+              <div>
+                <div className="grid justify-items-center items-center h-12 w-12 bg-muted border-border border rounded-lg">
+                  <OpenSource className="fill-primary h-8 w-8" />
+                </div>
+                <h1 className="mt-6 font-bold text-primary text-lg">Open Source</h1>
+                <p className="text-muted-foreground mt-2">I am passionate about open source, a collaborative approach to software development that fosters transparency, freedom, and community contributions. I believe in the power of collaborative innovation and aspire to be a catalyst for positive change, creating a more inclusive, sustainable, and innovative future.</p>
+              </div>
+              <div>
+                <div className="grid justify-items-center items-center h-12 w-12 bg-muted border-border border rounded-lg">
+                  <Leaf className="fill-primary h-8 w-8" />
+                </div>
+                <h1 className="mt-6 font-bold text-primary text-lg">Sustainable Technologies</h1>
+                <p className="text-muted-foreground mt-2"> Innovations that minimize environmental impact, conserve resources, and promote a greener future. They include renewable energy, energy efficiency, waste reduction, and eco-friendly materials. Balancing economic growth with environmental preservation, these technologies create a sustainable, resilient society, reducing emissions and promoting conservation.</p>
+              </div>
+              <div>
+                <div className="grid justify-items-center items-center h-12 w-12 bg-muted border-border border rounded-lg">
+                  <Innovation className="fill-primary h-8 w-8" />
+                </div>
+                <h1 className="mt-6 font-bold text-primary text-lg">Innovation</h1>
+                <p className="text-muted-foreground mt-2">Passionate about driving positive change through sustainable technologies. Embracing innovation, I strive to create greener, more efficient solutions that benefit society and the environment. Unleashing creative solutions and sustainable technologies to shape a brighter, more resilient world.</p>
+              </div>
+              <div>
+                <div className="grid justify-items-center items-center h-12 w-12 bg-muted border-border border rounded-lg">
+                  <Help className="fill-primary h-8 w-8" />
+                </div>
+                <h1 className="mt-6 font-bold text-primary text-lg">Helping Hand</h1>
+                <p className="text-muted-foreground mt-2">Compassionate support for those in need, fostering kindness, empowerment, and positive change. Through assistance, resources, and advocacy, it uplifts individuals and promotes social justice. Together, we create a brighter future, one act of care at a time.</p>
+              </div>
             </div>
           </div>
         </Section>
