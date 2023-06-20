6c00295d32f1cd075354ecd64c08dd27ef98b1c8
Sumiet Gore <sumeetgore96@gmail.com>
2023-06-20 15:08:41 +0200

    Removed edge runtime

--- app/blog/[slug]/page.js
+++ app/blog/[slug]/page.js
@@ -9,7 +9,6 @@ import fetchApi, { getPostBySlug, getSlugs } from "@/lib/fetchApi"
 import Markdown from 'react-markdown'
 import remarkGfm from 'remark-gfm'
 
-export const runtime = 'edge';
 export const dynaminc = 'force-static'
 export const cache = 'no-store'
 
