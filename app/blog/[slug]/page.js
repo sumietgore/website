a67b55d6ea3de25dd73c8ad72faff2dd887bb2d2
Sumiet Gore <sumeetgore96@gmail.com>
2023-06-20 14:34:32 +0200

    Added blog section with API Fetch

---
+++ app/blog/[slug]/page.js
@@ -0,0 +1,57 @@
+import { Header } from "@/components/shared/header"
+import { Button } from "@/components/ui/button"
+import Main from "@/components/ui/main"
+import Link from "next/link"
+import Image from "next/image"
+import Footer from "@/components/shared/footer"
+import Section from "@/components/ui/section"
+import fetchApi, { getPostBySlug, getSlugs } from "@/lib/fetchApi"
+import Markdown from 'react-markdown'
+import remarkGfm from 'remark-gfm'
+import P from "@/components/typography"
+
+
+export async function generateStaticParams() {
+    const response = await fetchApi(getSlugs, {
+        "pagination": {
+            "pageSize": 100
+        }
+    })
+
+    const posts = response.data.posts.data
+
+    return posts.map((post) => ({
+        slug: post.slug,
+    }))
+}
+
+export default async function Page({ params }) {
+
+    const response = await fetchApi(getPostBySlug, {
+        "filters": {
+            "slug": {
+                "eq": params.slug
+            }
+        }
+    })
+    const post = response.data.posts.data[0]
+
+    return (
+        <>
+            <Header />
+            <Main>
+                <Section className='md:mt-0'>
+                    <div className=" max-w-2xl mx-auto">
+                        <Button variant="ghost" asChild><Link href="/blog">Back to Posts</Link></Button>
+                        <div className="relative h-96 mt-8">
+                            <Image fill unoptimized src={post.attributes.image.data[0].attributes.url} />
+                        </div>
+                        <h1 className="text-4xl font-bold mt-12">{post.attributes.title}</h1>
+                        <Markdown children={post.attributes.content} rehypePlugins={[remarkGfm]} components={{ p: ({ node, ...props }) => <p className="mt-8 text-muted-foreground" {...props} /> }} />
+                    </div>
+                </Section>
+            </Main>
+            <Footer />
+        </>
+    )
+}
