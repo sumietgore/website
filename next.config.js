25dddb668dc0477f5161c23e434b57bb5d7c9f30
Sumiet Gore <sumeetgore96@gmail.com>
2023-06-20 15:16:10 +0200

    Added headers to next config

--- next.config.js
+++ next.config.js
@@ -4,6 +4,28 @@ const nextConfig = {
     images: {
         domains: ["https://web-assets.sumietgore.com", "strapi-assets.sumietgore.com"]
     },
+    async headers() {
+        return [
+            {
+                source: '/about',
+                headers: [
+                    {
+                        key: 'Cache-Control',
+                        value: 'public, max-age=604800, must-revalidate',
+                    },
+                ],
+            },
+            {
+                source: '/',
+                headers: [
+                    {
+                        key: 'Cache-Control',
+                        value: 'public, max-age=604800, must-revalidate',
+                    },
+                ],
+            },
+        ]
+    },
 }
 
 module.exports = nextConfig
