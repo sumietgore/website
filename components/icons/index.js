1629c181648a16851c5cf50a0dcd87586fb9b1ff
Sumiet Gore <sumeetgore96@gmail.com>
2023-08-07 15:21:00 +0200

    Add LinkedIn icon

--- components/icons/index.js
+++ components/icons/index.js
@@ -5,5 +5,6 @@ import OpenSource from "./openSource";
 import Leaf from "./leaf";
 import Help from "./help";
 import Innovation from "./innovation";
+import LinkedIn from "./linkedin"
 
-export { Logo, Github, Twitter, OpenSource, Leaf, Help, Innovation }
+export { Logo, Github, Twitter, OpenSource, Leaf, Help, Innovation, LinkedIn }
