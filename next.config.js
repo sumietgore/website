31d1d5ce12ab0500c583012bbae035aaae032768
Sumiet Gore <sumeetgore96@gmail.com>
2023-08-10 18:14:06 +0200

    chore: next config cleanup

--- next.config.js
+++ next.config.js
@@ -2,13 +2,6 @@
 const nextConfig = {
     distDir: 'build',
     "output": "export",
-    images: {
-        //unoptimized: true,
-        domains: ["https://web-assets.sumietgore.com", "https://strapi-assets.sumietgore.com"],
-        loader: 'custom',
-        loaderFile: './lib/imageLoader.js',
-        formats: ['image/avif', 'image/webp'],
-    },
 }
 
 module.exports = nextConfig
