84566967c35c8a8c529d0f1261a2168922ef5bce
Sumiet Gore <sumeetgore96@gmail.com>
2023-08-10 18:17:09 +0200

    fix: next image error

--- next.config.js
+++ next.config.js
@@ -2,6 +2,12 @@
 const nextConfig = {
     distDir: 'build',
     "output": "export",
+    images: {
+        domains: ["https://assets.sumietgore.com"],
+        loader: 'custom',
+        loaderFile: './lib/imageLoader.js',
+        formats: ['image/avif', 'image/webp'],
+    },
 }
 
 module.exports = nextConfig
