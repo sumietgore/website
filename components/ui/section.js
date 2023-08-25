787b39ac3079fb98ee81b05788bdc73a8b08fcbe
Sumiet Gore <sumeetgore96@gmail.com>
2023-08-25 09:17:06 +0200

    modified section ui

--- components/ui/section.js
+++ components/ui/section.js
@@ -2,7 +2,7 @@ import { twMerge } from "tailwind-merge"
 
 export default function Section({ children, className }) {
     return (
-        <section className={twMerge("mt-4 py-4 md:mt-12 md:py-14", className)}>
+        <section className={twMerge("mt-4 py-6 md:mt-14 md:py-10", className)}>
             {children}
         </section>
     )
